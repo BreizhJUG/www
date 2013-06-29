1a863fb8f92fa60249cee38c29b2c7e185fa18a6
Nicolas De Loof <nicolas.deloof@gmail.com>
2013-06-29 21:31:04 +0200

    initial yoeman setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('breizhjugApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
