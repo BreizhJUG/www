6adb87519570616a71b517730bda26897a5c30fa
Nicolas Morel <nmr.morel@gmail.com>
2013-08-04 17:37:03 +0200

    angularjs integration : speakers

--- javascripts/main.js
+++ javascripts/main.js
@@ -27,7 +27,7 @@ breizhjugApp.controller("homeController", function ($scope, $route, $location, $
     // change the url so we can move to a section even if the url didn't change
     $location.hash(':');
 
-    $scope.$on('$locationChangeSuccess', function(event) {
+    $scope.$on('$locationChangeSuccess', function (event) {
         // if we try to move to the same page, it means we want to go to a section of the page. we scroll to it and told angular it's the same route to not reload the page.
         if ($route.current.templateUrl.indexOf('home') > 0) {
             $location.hash($route.current.params.scrollTo);
@@ -45,12 +45,16 @@ breizhjugApp.controller("homeHeadController", function ($scope) {
     });
 });
 
-breizhjugApp.controller("homeNextController", function ($scope) {
-
+breizhjugApp.controller("homeNextController", function ($scope, Speakers) {
+    Speakers.fetchOne(1).then(function (resp) {
+        $scope.speaker = resp;
+    });
 });
 
 breizhjugApp.controller("homeEventsController", function ($scope) {
-    $('.eventsCarousel').bxSlider({
+    // initialize the slider
+    // FIXME an error occurs in angularjs and the next controllers are not initialized
+/*    $('.eventsCarousel').bxSlider({
         slideWidth: 250,
         minSlides: 1,
         maxSlides: 5,
@@ -58,11 +62,17 @@ breizhjugApp.controller("homeEventsController", function ($scope) {
         infiniteLoop: false,
         hideControlOnEnd: true,
         pager: false
-    });
+    });*/
 });
 
-breizhjugApp.controller("homeSpeakersController", function ($scope) {
-
+breizhjugApp.controller("homeSpeakersController", function ($scope, Speakers) {
+    Speakers.fetch().success(function (resp) {
+        resp.sort(function (a, b) {
+            return Math.random() - 0.5;
+        });
+        resp.length = 2;
+        $scope.speakers = resp;
+    });
 });
 
 breizhjugApp.controller("homeNewsController", function ($scope) {
@@ -77,6 +87,45 @@ breizhjugApp.controller("homeSponsorsController", function ($scope) {
 
 });
 
-breizhjugApp.controller("speakersController", function ($scope) {
+breizhjugApp.controller("speakersController", function ($scope, Speakers) {
+    Speakers.fetch().success(function (resp) {
+        $scope.speakers = resp;
+    });
+});
+
+breizhjugApp.factory("Speakers", function ($http, $q) {
+    var API_URI = '/data/speakers.json';
+
+    var fetch = function () {
+        return $http.get(API_URI, {cache: true});
+    };
+
+    return {
+
+        fetch: fetch,
+
+        fetchOne: function (id) {
+            var defer = $q.defer();
+            fetch().success(function (resp) {
+                var speaker;
+                for (var i = 0; i < resp.length; i++) {
+                    var tmp = resp[i];
+                    if (tmp.id == id) {
+                        speaker = resp[i];
+                        break;
+                    }
+                }
+                if (speaker) {
+                    defer.resolve(speaker);
+                } else {
+                    defer.reject('Not found');
+                }
+            }).error(function (resp) {
+                    defer.reject(resp);
+                });
+            return defer.promise;
+        }
+
+    };
 
 });
