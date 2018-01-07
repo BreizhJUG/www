56d42874e0ed244d65ccb126f35ee752dc8d97fe
Laurent HUET <laurent.huet@gmail.com>
2018-01-07 19:02:40 +0100

    add devoxx 2018 logo

--- javascripts/main.js
+++ javascripts/main.js
@@ -82,7 +82,7 @@ breizhjugApp.controller("homeController", function ($scope, $rootScope, Scroll) 
 breizhjugApp.controller("homeHeadController", function ($scope) {
     $scope.headers = [
         {src:"images/header/breizhjug-h300.jpg"},
-        {src:"images/header/devoxx_france_2017_600_130.png", href:"http://www.devoxx.fr"},
+        {src:"images/header/devoxx_france_v2_h_600_130_2018.png", href:"http://www.devoxx.fr"},
         {src:"images/header/BreizhCamp-h300.jpg", href:"http://www.breizhcamp.org"},
         {src:"images/header/GDG_Rennes-h300.jpg", href:"http://www.gdgrennes.org/"},
         {src:"images/header/docker_meetup.jpeg", href:"http://www.meetup.com/Docker-Rennes/events/"},
